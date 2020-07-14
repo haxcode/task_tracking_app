bf13ab4195800ccce921821536636cb1deead2e2
rkubica <rkubica@edokumenty.eu>
2020-07-14 22:27:16 +0200

    Add estimated time to todo model

--- lib/src/app/model/todo.dart
+++ lib/src/app/model/todo.dart
@@ -1,17 +1,21 @@
 
+import 'package:flutter/material.dart';
+
 class Todo {
 
   final int id;
   final String title;
   final String description;
+  final TimeOfDayFormat estimatedTime;
 
-  Todo({this.id, this.title, this.description});
+  Todo({this.id, this.title, this.description, this.estimatedTime});
 
   Map<String, dynamic> toMap() {
     return  {
       "id": id,
       "title":title,
       "description":description,
+      "estimatedTime":estimatedTime
     };
   }
 
